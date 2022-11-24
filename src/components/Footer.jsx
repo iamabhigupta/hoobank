cc860e5e6e576d5e9f6c21370ebd4ae1a400095a
Abhishek Gupta <abhishekgupta.july@gmail.com>
2022-11-24 09:53:49 +0530

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
 
       <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]">
         <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-          Copyright 2021 HooBank. All Rights Reserved.
+          Copyright &#169; 2021 HooBank. All Rights Reserved.
         </p>
         <div className="flex flex-row md:mt-0 mt-6">
           {socialMedia.map((social, index) => (
